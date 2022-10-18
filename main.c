21949802b8dffe27098880c0424c9ca69dd9f659
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-10-18 16:21:09 +0900

    07-02

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #1916513
-#include <stdio.h>  
-
-int main(void){
-	int flag = 1;
-	int y;
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int temp = 1;
 	
-	while(flag != 0)
-	{
-		y = 3;
-		flag = 0;
+	for (i=0;i<5;i++)
+	{	
+		printf("temp = %d\n", temp);
+		temp++;
 	}
-	y=4;
 	return 0;
 }
