2164c792a9d340c6e4c430538d31546d03a9045c
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-10-18 16:40:28 +0900

    07-03

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 int main(void)
 {
-	int i;
-	int temp = 1;
-	
-	for (i=0;i<5;i++)
-	{	
-		printf("temp = %d\n", temp);
-		temp++;
-	}
+	int i = 10;
+	printf("함수 호출전 i=%d\n", i);
+	i = inc(i);
+	printf("함수 호출후 i=%d\n", i);
 	return 0;
+}
+
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
